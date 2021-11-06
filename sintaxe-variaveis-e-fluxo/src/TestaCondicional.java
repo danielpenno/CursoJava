29482626332cd971717a7764854bf99c09a7eab4
Daniel Penno <danielpenno@gmail.com>
2021-11-06 16:17:14 -0300

    Algumas mudanças...

--- sintaxe-variaveis-e-fluxo/src/TestaCondicional.java
+++ sintaxe-variaveis-e-fluxo/src/TestaCondicional.java
@@ -1,24 +1,19 @@
 public class TestaCondicional {
-
     public static void main(String[] args) {
-        System.out.println("testando condicionais");
-
+        System.out.println("Testando condicionais...");
+        
         int idade = 16;
         int quantidadePessoas = 1;
 
         if (idade >= 18) {
-
-            System.out.println("Você tem mais que 18 anos");
-            System.out.println("Seja bem vindo");
+            System.out.println("Você tem mais que 18 anos.");
+            System.out.println("Seja bem-vindo(a).");
         } else {
             if (quantidadePessoas >= 2) {
-            	System.out.println("voce nao tem 18, mas pode entrar, " 
-            			+ "pois está acompanhado");
+            	System.out.println("Você não tem 18 anos, mas pode entrar, pois está acompanhado!");
             } else {
-
-                System.out.println("infelizmente voce nao pode entrar");
+                System.out.println("Infelizmente você não pode entrar.");
             }
-
         }
     }
 }
--- sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
+++ sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
@@ -1,18 +1,15 @@
 public class TestaCondicional2 {
-
 	public static void main(String[] args) {
-		System.out.println("testando condicionais");
+		System.out.println("Testando condicionais...");
 
 		int idade = 16;
 		int quantidadePessoas = 3;
 		boolean acompanhado = quantidadePessoas >= 2;
 
 		if (idade >= 18 && acompanhado) {
-
-			System.out.println("Seja bem vindo");
+			System.out.println("Seja bem-vindo(a).");
 		} else {
-			System.out.println("infelizmente voce nao pode entrar");
+			System.out.println("Infelizmente você não pode entrar.");
 		}
-
 	}
 }
